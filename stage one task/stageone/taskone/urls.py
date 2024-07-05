f6bd087cfb4f0a7f11f8b2253de20309253c7b8b
Abraham-Franklin <110688915+Abraham-Franklin@users.noreply.github.com>
2024-07-05 10:32:28 +0100

    Update urls.py

--- stage one task/stageone/taskone/urls.py
+++ stage one task/stageone/taskone/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getData),
+    # path('', views.getData),
     path('api/hello/', views.hello, name="hello"),
 ]
